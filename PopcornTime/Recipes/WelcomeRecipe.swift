e1a05102abd92174030f7bb743993ec17cd9ce36
RefusedFlow <jakeround@jakes-mbp.home>
2016-05-05 00:08:44 +0100

    Redesigned Welcome Screen
    Implemented New Welcome Screen layout to display movies and tv show
    watchlist.

--- PopcornTime/Recipes/WelcomeRecipe.swift
+++ PopcornTime/Recipes/WelcomeRecipe.swift
@@ -9,32 +9,27 @@
 import TVMLKitchen
 import PopcornKit
 
-struct PreviewItem {
-    var fanartImage: String!
-
-    init(fanartImage: String) {
-        self.fanartImage = fanartImage
-    }
-}
-
 public struct WelcomeRecipe: RecipeType {
-
+    
     public let theme = DefaultTheme()
     public let presentationType = PresentationType.Default
-
+    
     let title: String
-    let items: [PreviewItem]
-
-    init(title: String, items: [PreviewItem]) {
+    let movies: [WatchItem]
+    let shows: [WatchItem]
+    
+    init(title: String, movies: [WatchItem], shows: [WatchItem]) {
         self.title = title
-        self.items = items
+        self.movies = movies
+        self.shows = shows
     }
-
+    
     init(title: String) {
         self.title = title
-        self.items = []
+        self.movies = []
+        self.shows = []
     }
-
+    
     public var xmlString: String {
         var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
         xml += "<document>"
@@ -42,25 +37,41 @@ public struct WelcomeRecipe: RecipeType {
         xml += "</document>"
         return xml
     }
-
-    public var topShowsAndMovies: String {
-        let mapped: [String] = items.map {
-            return "<img src=\"\($0.fanartImage)\"/>"
+    
+    public var movieWatchList: String {
+        let mapped: [String] = movies.map {
+            var string = "<lockup actionID=\"showMovie»\($0.id)\">"
+            string += "<img src=\"\($0.coverImage)\" width=\"250\" height=\"375\" />"
+            string += "<title class=\"hover\">\($0.name.cleaned)</title>"
+            string += "</lockup>"
+            return string
         }
-        return mapped.joinWithSeparator("")
+        return mapped.joinWithSeparator("\n")
     }
-
+    
+    public var showWatchList: String {
+        let mapped: [String] = shows.map {
+            var string = "<lockup actionID=\"showMovie»\($0.id)\">"
+            string += "<img src=\"\($0.coverImage)\" width=\"250\" height=\"375\" />"
+            string += "<title class=\"hover\">\($0.name.cleaned)</title>"
+            string += "</lockup>"
+            return string
+        }
+        return mapped.joinWithSeparator("\n")
+    }
+    
     public var template: String {
         var xml = ""
         if let file = NSBundle.mainBundle().URLForResource("WelcomeRecipe", withExtension: "xml") {
             do {
                 xml = try String(contentsOfURL: file)
-                xml = xml.stringByReplacingOccurrencesOfString("{{TOP_SHOWS_MOVIES}}", withString: topShowsAndMovies)
+                xml = xml.stringByReplacingOccurrencesOfString("{{MOVIES_WATCHLIST}}", withString: movieWatchList)
+                xml = xml.stringByReplacingOccurrencesOfString("{SHOWS_WATCHLIST}}", withString: showWatchList)
             } catch {
                 print("Could not open Catalog template")
             }
         }
         return xml
     }
-
+    
 }

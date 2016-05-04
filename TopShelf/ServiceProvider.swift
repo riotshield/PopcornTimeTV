fec7fa7d345360812a87eaa5150e400d7dbfd6f2
Buzzel <yogi.bear.development@gmail.com>
2016-05-04 14:19:28 -0700

    Clean up and fix the top shelf

--- TopShelf/ServiceProvider.swift
+++ TopShelf/ServiceProvider.swift
@@ -12,6 +12,10 @@ import PopcornKit
 
 class ServiceProvider: NSObject, TVTopShelfProvider {
 
+    let manager = NetworkManager.sharedManager()
+    
+    var items = [TVContentItem]()
+    
     override init() {
         super.init()
     }
@@ -22,52 +26,50 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         // Return desired Top Shelf style.
         return .Sectioned
     }
-    
-    var items: [TVContentItem] = []
 
     var topShelfItems: [TVContentItem] {
-        let manager = NetworkManager.sharedManager()
+        self.items.removeAll()
         
         let semaphore = dispatch_semaphore_create(0)
         
         manager.fetchServers { servers, error in
             if let servers = servers {
                 if let yts = servers["yts"] as? [String], let eztv = servers["eztv"] as? [String] {
-                    manager.setServerEndpoints(yts: yts.first!, eztv: eztv.first!)
-                    
-                    manager.fetchShowsForPage(1) { shows, error in
-
+                    self.manager.setServerEndpoints(yts: yts.first!, eztv: eztv.first!)
+                    self.manager.fetchShowsForPage(1) { shows, error in
                         if let shows = shows {
-                            var ShowItems = [TVContentItem]();
-                            manager.fetchMovies(limit: 10, page: 1, quality: "1080p", minimumRating: 3, queryTerm: nil, genre: nil, sortBy: "seeds", orderBy: "desc", withImages: true) { movies, error in
+                            self.manager.fetchMovies(limit: 10, page: 1, quality: "1080p", minimumRating: 3, queryTerm: nil, genre: nil, sortBy: "seeds", orderBy: "desc", withImages: true) { movies, error in
                                 if let movies = movies {
-                                    var MovieItems = [TVContentItem]();
-
+                                    
+                                    // Movies
+                                    var movieItems = [TVContentItem]()
                                     for movie in movies {
-                                        MovieItems.append(
+                                        movieItems.append(
                                             self.buildShelfItem(
                                                 movie.title.cleaned,
-                                                Image: movie.mediumCoverImage,
-                                                Action: "showMovie/\(String(movie.id))"
+                                                image: movie.mediumCoverImage,
+                                                action: "showMovie/\(String(movie.id))"
                                             )
                                         )
                                     }
-                                  
+                                    
                                     let latestMoviesSectionTitle = "Latest Movies"
                                     let latestMovieSectionItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: latestMoviesSectionTitle, container: nil)!)
                                     latestMovieSectionItem!.title = latestMoviesSectionTitle
-                                    latestMovieSectionItem!.topShelfItems = MovieItems
-
+                                    latestMovieSectionItem!.topShelfItems = movieItems
+                                    
                                     self.items.append(latestMovieSectionItem!)
-
+                                    
                                 }
                                 
+                                // Shows
+                                var showItems = [TVContentItem]()
                                 for show in shows[0..<10] {
-                                    ShowItems.append(
+                                    showItems.append(
                                         self.buildShelfItem(
                                             show.title.cleaned,
-                                            Image: show.posterImage,
-                                            Action:  "showShow/\(show.id)/\(show.title.slugged)/\(show.tvdbId)"
+                                            image: show.posterImage,
+                                            action: "showShow/\(show.id)/\(show.title.slugged)/\(show.tvdbId)"
                                         )
                                     )
                                 }
@@ -75,7 +77,7 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
                                 let popularShowsSectionTitle = "Popular Shows"
                                 let popularShowSectionItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: popularShowsSectionTitle, container: nil)!)
                                 popularShowSectionItem!.title = popularShowsSectionTitle
-                                popularShowSectionItem!.topShelfItems = ShowItems
+                                popularShowSectionItem!.topShelfItems = showItems
                                 
                                 self.items.append(popularShowSectionItem!)
                                 
@@ -91,14 +93,13 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         return self.items
     }
     
-    func buildShelfItem(Title: String, Image: String, Action: String) -> TVContentItem
-    {
-        let item = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: Title, container: nil)!)
-        item!.imageURL = NSURL(string:Image)
+    func buildShelfItem(title: String, image: String, action: String) -> TVContentItem {
+        let item = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: title, container: nil)!)
+        item!.imageURL = NSURL(string: image)
         item!.imageShape = .Poster
-        item!.displayURL = NSURL(string: "PopcornTimeTV://\(Action)");
-        item!.playURL = NSURL(string: "PopcornTimeTV://\(Action)");
-        item!.title = Title
+        item!.displayURL = NSURL(string: "PopcornTimeTV://\(action)")
+        item!.playURL = NSURL(string: "PopcornTimeTV://\(action)")
+        item!.title = title
         return item!
     }
 

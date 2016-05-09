86c9daff55d4c9753e0be55a4f839315db162e29
Buzzel <yogi.bear.development@gmail.com>
2016-05-09 11:50:48 -0700

    progress: Add a bit more detail to the progress screen

--- PopcornTime/View Controllers/ProgressViewController.swift
+++ PopcornTime/View Controllers/ProgressViewController.swift
@@ -18,7 +18,9 @@ class ProgressViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
-
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var statsLabel: UILabel!
+    
     var magnet: String!
     var imageAddress: String!
     var backgroundImageAddress: String!
@@ -36,8 +38,10 @@ class ProgressViewController: UIViewController {
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         if let _ = magnet, let _ = movieName, let _ = imageAddress, let _ = backgroundImageAddress {
+            statsLabel.text = ""
+            percentLabel.text = "0%"
             nameLabel.text = "Processing " + movieName + "..."
             imageView.kf_setImageWithURL(NSURL(string: imageAddress)!)
             backgroundImageView.kf_setImageWithURL(NSURL(string: backgroundImageAddress)!)
@@ -48,12 +52,18 @@ class ProgressViewController: UIViewController {
 
             PTTorrentStreamer.sharedStreamer().startStreamingFromFileOrMagnetLink(magnet, progress: { status in
                 self.downloading = true
+                
+                self.percentLabel.text = "\(Int(status.bufferingProgress * 100))%"
+                
+                let speedString = NSByteCountFormatter.stringFromByteCount(Int64(status.downloadSpeed), countStyle: .Binary)
+                self.statsLabel.text = "Speed: \(speedString)/s  Seeds: \(status.seeds)  Peers: \(status.peers)"
+                
                 self.progressView.progress = status.bufferingProgress
-                if self.progressView.progress <= 0.0 {
+                if self.progressView.progress > 0.0 {
                     self.nameLabel.text = "Buffering " + self.movieName + "..."
                 }
             }, readyToPlay: { url in
-                self.playVLCVideo(url, hash: self.magnet)
+                self.playVLCVideo(url, hash: "")
             }) { error in
                 print(error)
             }
